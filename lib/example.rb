344887126f72dae0404a7f23290008eb725a0d5b
Mori Reo <reoring@craftsman-software.com>
2016-02-08 20:19:05 +0900

    Remove token

--- lib/example.rb
+++ lib/example.rb
@@ -1,5 +1,5 @@
 require 'Orizuru'
-client = Orizuru.create('http://192.168.50.4:8080', 'a99b101ccea0d8327831c4a12472c7cc8fbb425389772afc56862307277ef9b3')
+client = Orizuru.create('http://192.168.50.4:8080', 'Use your tsuru token')
 
 client.apps.each do |app|
   p app.name
